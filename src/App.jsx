0e3ac6708d996bed1ccf9a07ec70ae30127509b8
MusabLodhi <musabdev65@gmail.com>
2024-12-30 15:27:39 +0500

    add dark&light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Navigation from "./components/Navigation/Navigation";
 
 function App() {
+  const [isDark, setIsDark] = useState(true);
 
+  const changeMode = () => {
+    setIsDark(!isDark)
+    document.body.setAttribute("data-theme",  isDark ? "dark" : "light"); 
+  };
   return (
     <>
     <div className="container">
-       <Header />
+       <Header modeF={changeMode} currentMode={isDark} />
      </div>
        <Hero />
       <div className="container">
