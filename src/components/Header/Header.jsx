ed9a6215b239091c7a8b0387fe9c5f390a97250b
MusabLodhi <musabdev65@gmail.com>
2024-12-30 12:35:08 +0500

    header ui

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,22 @@ import pawLogo from './logo.png'
 
 const Header = () => {
 
-    return(
+    return (
         <nav>
             <div className={styles.logo}>
                 <img src={pawLogo} alt="paw_logo" />
-              <h3>Catopia</h3>
+                <h3>Catopia</h3>
+            </div>
+            <div className={styles.nav_ctas}>
+                <a href='#'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                </svg>
+                    Your Favorites
+                </a>
+                <p><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                </svg>
+                </p>
             </div>
         </nav>
     )
