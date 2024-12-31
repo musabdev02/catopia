0f216841b26eafed0090df09804ea7f90ef98c96
MusabLodhi <musabdev65@gmail.com>
2024-12-31 11:47:26 +0500

    fix dark & light mode

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,19 +5,22 @@ import CatFact from '../CatFact/CatFact';
 
 const Navigation = () => {
     const [activeButton, setActiveButton] = useState("Random Cats");
-    const pages = [<RandomCat />, <CatFact />];
     const handleClick = (buttonName) =>{
         setActiveButton(buttonName);
     };
+    let content = "";
+    if(activeButton === "Random Cats"){
+        content = <RandomCat />;
+    }else if(activeButton === "Cats Fun Facts"){
+        content = <CatFact />
+    }
     return (<>
         <div className={styles.navigation}>
             <button className={activeButton === "Random Cats" ? styles.navActive : ""} onClick={() => handleClick("Random Cats")}>Random Cats</button>
             <button className={activeButton === "Cats Fun Facts" ? styles.navActive : ""} onClick={() => handleClick("Cats Fun Facts")}>Cats Fun Facts</button>
             <button className={activeButton === "Generate Cat Names" ? styles.navActive : ""} onClick={() => handleClick("Generate Cat Names")}>Generate Cat Names</button>
         </div>
-        {
-            activeButton === "Random Cats" ? <CatFact /> : ""
-        }
+        {content}
         
     </>
     )
